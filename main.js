dd2b1c01616c0b305669b3361de799722ae4a5da
Shutaro <tsyutaro0121@gmail.com>
2021-05-17 00:10:17 +0900

    動作の軽量化

--- main.js
+++ main.js
@@ -48,10 +48,25 @@ function make_text_change(type){
     }
 }
 
+var Lazy = Boolean(false);
+function Lazy_loading(){
+    if(Lazy == false){
+        Lazy = true;
+        var twitte_script = document.createElement('script');
+        twitte_script.src = 'https://platform.twitter.com/widgets.js';
+        document.getElementById('twitter-js').appendChild(twitte_script);
+
+        document.getElementById("google-map").innerHTML = '<iframe width="100%" height="315" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d520.1727181159928!2d141.49508712121485!3d42.62426748863378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f7514173752d159%3A0xc62ed098a3f2a308!2z44CSMDU5LTEyNzUg5YyX5rW36YGT6Iur5bCP54mn5biC6Yym5bKhIOaWh-enkeezu-OCteODvOOCr-ODq-WupA!5e0!3m2!1sja!2sjp!4v1617391725063!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+    }
+}
+
 window.addEventListener('scroll', function(){
+    Lazy_loading();
     scroll();
 });
-
+window.addEventListener('touchstart', function(){
+    Lazy_loading();
+});
 
 var notification_state = Boolean(false);
 var make1 = Boolean(false);
@@ -135,4 +150,22 @@ function scroll(){
             document.getElementById("link").style.opacity=1;
         },300);
     }
+}
+
+function youtube(iframe_url,sub_url){
+    var display = '<iframe width="100%" height="90%" src="'+iframe_url+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    display += '<a href="'+sub_url+'" target="_blank" rel="noopener">閲覧できない場合はこちらからYouTubeを開いてください。<i class="fas fa-external-link-alt"></i></a>';
+    document.getElementById("iframe-window-contents").innerHTML = display;
+
+    document.getElementById("iframe-window").style.display = "block";
+    document.getElementById("iframe-window").animate([{opacity: '0'}, {opacity: '1'}], 500);
+    document.getElementById("iframe-window").style.opacity=1;
+}
+
+function close_window(){
+    document.getElementById("iframe-window").animate([{opacity: '1'}, {opacity: '0'}], 200);
+    window.setTimeout(function() {
+        document.getElementById("iframe-window").style.opacity=0;
+        document.getElementById("iframe-window").style.display = "none";
+    },200);
 }
