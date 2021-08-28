9ab2aeb8189a34f0085d48b306f373ae2b0753d6
Shutaro <tsyutaro0121@gmail.com>
2021-08-28 22:52:37 +0900

    サイトの更新
    ・ロボットの年表の追加

---
+++ index.js
@@ -0,0 +1,134 @@
+//IE警告(IEはWebp表示不可)
+if(window.navigator.userAgent.indexOf('Trident') != -1 || window.navigator.userAgent.indexOf('MSIE') != -1){ 
+  if(confirm('Internet Explorerでの閲覧は推奨していません。\n Microsoft Edge または Google Chrome、FireFox、Operaなどのブラウザの利用を推奨しています。\n Google Chromeのダウンロードページに遷移しますか?')==true) {
+        location.href="https://www.google.co.jp/chrome/";
+    } else {
+    }
+}
+
+/*初期読み込み時動作*/
+//ロード完了時
+window.onload = function(){
+    window.setTimeout(function() {
+        make_text_change();                 //「作る」のアニメーション
+        Scroll();                           //初期の高さで表示する必要があるものがある場合のために読み込む
+    },100);                                 //100ms待機後{}内実行
+}
+
+//「作る」のアニメーション
+function make_text_change(){
+    var make = document.getElementById("make-text");
+    make.innerHTML = "造る";
+    window.setTimeout(function() {
+        make.innerHTML = "作る";
+        window.setTimeout(function() {
+            make.innerHTML = "創る";
+            document.getElementById("strong").animate([{opacity: '0'}, {opacity: '1'}], 800)
+            document.getElementById("strong").style.opacity=1;
+            window.setTimeout(function() {
+                document.getElementById("robot").animate([{opacity: '0'}, {opacity: '1'}], 800)
+                document.getElementById("robot").style.opacity=1;
+            },400);
+        },100);
+    },100);
+}
+
+
+/*遅延読み込み*/
+var Lazy = Boolean(false); //遅延読み込み false：未完了
+function Lazy_loading(){
+    if(Lazy == false){
+        Lazy = true;
+        /*Twitter*/
+        var twitte_script = document.createElement('script');
+        twitte_script.src = 'https://platform.twitter.com/widgets.js';
+        document.getElementById('twitter-js').appendChild(twitte_script);
+
+        /*Instagram*/
+        var instagram = document.createElement('script');
+        instagram.src = 'https://snapwidget.com/js/snapwidget.js';
+        document.getElementById('instagram-iframe').appendChild(instagram);
+        document.getElementById('instagram-iframe').innerHTML = '<script src="https://snapwidget.com/js/snapwidget.js"></script><iframe title="snapwidget Instagram" src="https://snapwidget.com/embed/937227" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:100%; "></iframe>';
+
+        /*Google Maps*/
+        document.getElementById("google-map").innerHTML = document.getElementById("google-map").innerHTML + '<iframe width="100%" height="315" title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d520.1727181159928!2d141.49508712121485!3d42.62426748863378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f7514173752d159%3A0xc62ed098a3f2a308!2z44CSMDU5LTEyNzUg5YyX5rW36YGT6Iur5bCP54mn5biC6Yym5bKhIOaWh-enkeezu-OCteODvOOCr-ODq-WupA!5e0!3m2!1sja!2sjp!4v1617391725063!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+    }
+}
+
+
+/*遅延読み込み / アニメーション トリガー*/
+window.addEventListener('scroll', function(){ //スクロールした場合に動作
+    /*スクロール*/
+    Lazy_loading(); //遅延読み込み
+    Scroll();       //スクロールアニメーション
+});
+window.addEventListener('touchstart', function(){ //画面をタップした場合に動作
+    /*画面タップ*/
+    Lazy_loading(); //遅延読み込み
+});
+
+
+//アニメーション表示状態 false = 非表示
+var notification_state = Boolean(false);
+var instagram_state = Boolean(false);
+var activity = Boolean(false);
+var place = Boolean(false);
+var link = Boolean(false);
+
+//スクロールアニメーション
+function Scroll(){
+    //スクロール時,初期読み込み動作
+    let browser = window.innerHeight;   //ブラウザの高さ
+    var scroll = window.pageYOffset;    //スクロール量
+    var scroll_under = browser + scroll;//表示されている一番下の高さ
+
+    window.setTimeout(function() {
+        if((notification_state == false) && (scroll_under > (window.pageYOffset +document.getElementById("notification").getBoundingClientRect().top))){
+            //notificationのアニメーション
+            notification_state = true;
+            document.getElementById("notification").style.opacity=1;
+        }
+        if((instagram_state == false) && (scroll_under > (window.pageYOffset +document.getElementById("instagram").getBoundingClientRect().top))){
+            //instagramのアニメーション
+            instagram_state = true;
+            document.getElementById("instagram").style.opacity=1;
+        }
+        if((activity == false) && (scroll_under > (window.pageYOffset + document.getElementById("activity-inner-box-outer").getBoundingClientRect().top))){
+            //activity(フェードイン)
+            activity = true;
+            document.getElementById("activity-inner-box-outer").style.opacity = 1;
+        }
+        if((place == false) && (scroll_under > (window.pageYOffset + document.getElementById("place").getBoundingClientRect().top))){
+            //placeのアニメーション
+            place = true;
+            document.getElementById("place").style.opacity=1;
+        }
+        if((link == false) && (scroll_under > (window.pageYOffset + document.getElementById("link").getBoundingClientRect().top))){
+            //linkのアニメーション
+            link = true;
+            document.getElementById("link").style.opacity=1;
+        }
+    },250);  //250ms後に表示
+}
+
+
+/*YouTubeホップアップ*/
+function youtube(iframe_url,sub_url){
+    //YouTubeリンク押下時　ホップアップ表示
+    var display = '<iframe width="100%" height="90%" src="'+iframe_url+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    display += '<a class="youtube-info" href="'+sub_url+'" target="_blank" rel="noopener">閲覧できない場合はこちらからYouTubeを開いてください。<i class="fas fa-external-link-alt"></i></a>';
+    document.getElementById("iframe-window-contents").innerHTML = display;
+
+    document.getElementById("iframe-window").style.display = "block";
+    document.getElementById("iframe-window").animate([{opacity: '0'}, {opacity: '1'}], 500);
+    document.getElementById("iframe-window").style.opacity=1;
+}
+
+function close_window(){
+    //ホップアップ終了
+    document.getElementById("iframe-window").animate([{opacity: '1'}, {opacity: '0'}], 200);
+    window.setTimeout(function() {
+        document.getElementById("iframe-window").style.opacity=0;
+        document.getElementById("iframe-window").style.display = "none";
+    },200);
+}
